1a3ccb272a9cc12e541e00b5e3bcf196cbdc1a64
Guilherme Barbosa Pereira <guilherme.pereira@18digital.com.br>
2018-12-26 14:29:45 -0200

    Saving working in progress at the details page

--- lib/ui/details/details_page.dart
+++ lib/ui/details/details_page.dart
@@ -9,33 +9,63 @@ class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Stack(
-            children: <Widget>[
-              Positioned(
-                top: 0,
-                right: 0,
-                left: 0,
-                              child: Container(
-                  height: MediaQuery.of(context).size.height * 0.6,
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(news.getBackgroundImage())
-                    )
-                  ),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(news.getBackgroundImage()))),
+                ),
+                AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0.0,
+                )
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: ListView(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Ask a banner',
+                          style: Theme.of(context).textTheme.body1.copyWith(
+                                color: news.getTagColor(),
+                                fontSize: 11.0,
+                                fontWeight: FontWeight.w900,
+                              ),
+                        ),
+                        Text(
+                          '7 min read',
+                          style: Theme.of(context).textTheme.body1.copyWith(
+                              color: Color(0XFFb8bfc9), fontSize: 11.0),
+                        )
+                      ],
+                    ),
+                    Text(
+                      'How not to fail at and improve the customer experience',
+                      style: Theme.of(context).textTheme.headline.copyWith(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w900,
+                          ),
+                    ),
+                    Text(news.detailsText)
+                  ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 30.0),
-                child: IconButton(
-                  onPressed: () {Navigator.of(context).pop();},
-                  icon: Icon(Icons.arrow_back),
-                ),
-              )
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
